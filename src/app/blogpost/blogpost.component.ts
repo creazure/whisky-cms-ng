ca86f7856f7adbb0de2f812cc91d29f0891a6f0a
Anis AJMI <anis.a@soloinvest.com>
2020-08-07 16:49:09 +0200

    Add new module for material design + 2 components blogpost & blogpost-list

---
+++ src/app/blogpost/blogpost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blogpost',
+  templateUrl: './blogpost.component.html',
+  styleUrls: ['./blogpost.component.sass']
+})
+export class BlogpostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
