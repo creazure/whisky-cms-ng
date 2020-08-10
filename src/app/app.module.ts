a8e384445139404ad65fdfddadc5de7358ebe060
Anis AJMI <anis.a@soloinvest.com>
2020-08-10 14:32:53 +0200

    Display Blogpost list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BlogpostComponent } from './blogpost/blogpost.component';
 import { BlogpostListComponent } from './blogpost-list/blogpost-list.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { BlogpostListComponent } from './blogpost-list/blogpost-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/blogpost-list/blogpost-list.component.ts
+++ src/app/blogpost-list/blogpost-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import {Blogpost} from '../models/blogpost';
+import { BlogpostService } from '../blogpost.service';
 
 @Component({
   selector: 'app-blogpost-list',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blogpost-list.component.sass']
 })
 export class BlogpostListComponent implements OnInit {
+  blogPostList$: Observable<Blogpost[]>;
 
-  constructor() { }
+  constructor(private blogpostService: BlogpostService) { }
 
   ngOnInit() {
+    this.blogPostList$ =  this.blogpostService.getBlocposts()
   }
 
 }
